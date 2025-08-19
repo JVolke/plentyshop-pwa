5f483b3810a6e580a3502ddb35794adeade71dc9
Jens Volke <desire.add@gmail.com>
2025-08-19 09:10:51 +0200

    Update default.vue

--- apps/web/layouts/default.vue
+++ apps/web/layouts/default.vue
@@ -11,8 +11,8 @@
     <Cookiebar />
     <PreviewMode />
     <NuxtLazyHydrate when-visible>
-      <FooterBlock v-if="runtimeConfig.public.isDev && !route.meta.isBlockified" />
-      <UiFooter v-if="!runtimeConfig.public.isDev" />
+      <!-- FooterBlock v-if="runtimeConfig.public.isDev && !route.meta.isBlockified" /-->
+      <UiFooter  />
     </NuxtLazyHydrate>
     <QuickCheckout v-if="isOpen" :product="product" />
   </div>
