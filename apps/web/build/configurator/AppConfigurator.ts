0cda182613f5519ce74aedaa5bab017d57c80942
Jens Volke <desire.add@gmail.com>
2025-11-14 09:28:12 +0100

    fix

--- apps/web/build/configurator/AppConfigurator.ts
+++ apps/web/build/configurator/AppConfigurator.ts
@@ -84,7 +84,7 @@ export class AppConfigurator {
       `Generating language files with languages ${languages.activated} and default language ${languages.default}...`,
     );
 
-    const languageFilesPath = path.resolve(__dirname, '../../i18n/lang');
+    const languageFilesPath = path.resolve(__dirname, '../../app/lang');
     const defaultLanguageFile = path.resolve(languageFilesPath, `${languages.default}.json`);
 
     this.writeLanguageFiles(defaultLanguageFile, languages, languageFilesPath);
